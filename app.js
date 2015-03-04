e9b320c93446104a96dd126212358e7a15767ab3
David Christian <david.christian@Services-MacBook-Pro.local>
2015-03-04 10:11:24 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+// GET /static/style.css etc.
+app.use('/app', express.static(__dirname + '/app'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Handles all other requests by redirecting the browser back to the root path
+// and tacking on URL's path as the fragment; e.g. "/foo/" => "/#/foo/".
+app.get('*', function (req, res) {
+    res.redirect('/#' + req.url);
+});
+
+app.listen(3000);
+
+console.log("  Listening on : 3000")
