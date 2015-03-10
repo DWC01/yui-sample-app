1e58f5e7daff868f1b9c6da063b5c0997da077c7
dwc01 <david.w.christian@gmail.com>
2015-03-09 18:58:30 -0700

    added wf-form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,21 +28,49 @@
             'transactionModel', 'transactionList', 'transactionApp', 'transactionView', 'localStorageSync',
             'wf2-button', 'wf2-form', 'wf2-global-header', 'wf2-gridsmanager', function(Y) {
 
+    // Page Header
     Y.WF2.GlobalHeader.getGlobalHeader({
       appname: 'Global Wires'    
     }).render();
 
-    // Instantiate 'GridsManager' for auto height-adjustment.
+    // Instantiate Grid Layout
     new Y.WF2.GridsManager({
       gridsContainer: '#grids'
     });
 
-    var app = new TransactionApp();
 
+
+    // Setup App Details / Render
+    var app = new TransactionApp({container: '#app-flow'});
     app.route('/', function(req) {
       app.showView('transaction');
-    });
+      
+      var formFields = {};
+      
+      formFields.email = new Y.WF2.FORM.FieldTextInput({
+          labelText   : "Email Address",
+          hint        : "E.G. foo@bar.com",
+          controlName : "email",
+          validators  : [ "isEmailAddr", "isRequired" ] //array of validators
+      });
+      
+      formFields.desc = new Y.WF2.FORM.FieldTextArea({
+          labelText   : "Description",
+          counterMax  : 20,
+          placeholder : "stay brief",
+          controlName : "desc"
+      });
 
+      var form = new Y.WF2.FORM.Form({
+          formAction  : "http://wnl-svr167.wellsfargo.com:8020/echorequest/",
+          subfields   : formFields
+      }).render("#transactionForm");
+      
+      Y.Object.each( formFields, function(subfieldWidget,subfieldRole){
+          subfieldWidget.render( form.get('controlNode') );
+      });
+      
+    });
     app.render().dispatch();
 
 });
